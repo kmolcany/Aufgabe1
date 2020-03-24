01569e74e76c7f2916ff461a9f9699d623224778
Karin Molcany <karin.molcany@gmail.com>
2020-03-24 23:08:23 +0100

    Initial commit

---
+++ src/HelloWorld.java
@@ -0,0 +1,9 @@
+public class HelloWorld {
+    public static void main(String [] args) {
+        System.out.println("Helo World");
+        int i  = 2;
+        int d = 1;
+        int x = d*i;
+        System.out.println("Campus 0" + x);
+    }
+}
